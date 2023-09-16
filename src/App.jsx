fb526018a12aae496e74428181ab45194c434db1
shivansh13579 <shivansh0975@gmail.com>
2023-09-16 21:52:37 +0530

    custom hooks done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p className='bg-yellow-400 text center px-2 py-1'>Password Generator </p>
+    </>
+  )
+}
+
+export default App
